3b1040037e1a27417839ea9550193f3f7aa04ea7
Moira Leon <moira.leon2@gmail.com>
2021-09-27 22:04:16 -0700

    adding variables for ui interaction

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -2,9 +2,32 @@ package com.example.weather_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    final String API_KEY ="89e5ced9260f0649a78adcc4aab39776";
+    final String WEATHER_URL ="https://home.openweathermap.org/data/2.5/weather";
+
+    final long MIN_TIME=5000;
+    final float MIN_DISTANCE = 1000;
+    final int REQUEST_CODE=101;
+
+    String Location_Provider = LocationManager.GPS_PROVIDER;
+
+    TextView NameofCity, weatherState, Temperature;
+    ImageView mweatherIcon;
+
+    RelativeLayout mCityFinder;
+
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

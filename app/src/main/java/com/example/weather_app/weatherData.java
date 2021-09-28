d0c3899dddc4315678dbb102015058cfa40bd51f
Moira Leon <moira.leon2@gmail.com>
2021-09-28 12:38:52 -0700

    correct getters and setters, images returned with codes, and °C added to getter

--- app/src/main/java/com/example/weather_app/weatherData.java
+++ app/src/main/java/com/example/weather_app/weatherData.java
@@ -24,7 +24,64 @@ public class weatherData {
 
         } catch (JSONException e) {
             e.printStackTrace();
+            return null;
         }
     }
 
+
+    private static String updateWeatherIcon(int condition){
+        if(condition>=0 && condition <=300){
+            return "thunderstorm";
+        }
+        else if(condition>=300 && condition <=500){
+            return "rain";
+            //light rain
+        }
+        else if(condition>=500 && condition <=600){
+            return "rain";
+            //shower
+        }
+        else if(condition>=701 && condition <=771){
+            return "fog";
+        }
+        else if(condition>=772 && condition <=800){
+            return "overcast";
+        }
+        else if(condition==800){
+            return "sun";
+        }
+        else if(condition>=801 && condition <=804){
+            return "cloudy";
+        }
+        else if(condition>=900 && condition <=902){
+            return "thunderstorm";
+        }
+        else if(condition==903){
+            return "snow";
+        }
+        else if(condition==904){
+            return "sun";
+        }
+        else if(condition>=905 && condition <=1000){
+            return "thunderstorm";
+        }
+        return "unknown";
+
+    }
+
+    public String getmTemperature() {
+        return mTemperature +"°C";
+    }
+
+    public String getmIcon() {
+        return mIcon;
+    }
+
+    public String getmCity() {
+        return mCity;
+    }
+
+    public String getmWeatherType() {
+        return mWeatherType;
+    }
 }
